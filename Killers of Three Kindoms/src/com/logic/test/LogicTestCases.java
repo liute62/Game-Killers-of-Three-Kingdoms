9ae37128ccac891ef51a4a2ddc43eadeaaceceff
Max_Liu <liute62@sina.cn>
2015-04-03 14:46:55 -0400

    Test for the number of available cards is ten. It is also success, we don't need to change the method.

--- Killers of Three Kindoms/src/com/logic/test/LogicTestCases.java
+++ Killers of Three Kindoms/src/com/logic/test/LogicTestCases.java
@@ -18,7 +18,7 @@ public class LogicTestCases {
 	/**Test for class APlayer getAvailableCards()**/
 	List<ACard> generateTestCards(int num){
 		List<ACard> list = new ArrayList<ACard>();
-		if(num == 1){
+		for (int i = 0; i < num; i++) {
 			ACard card = null;
 			list.add(card);
 		}
@@ -27,7 +27,7 @@ public class LogicTestCases {
 	
 	List<ACard> generateAvailableCards(int num){
 		List<ACard> list = new ArrayList<ACard>();
-		if(num == 1){
+		for (int i = 0; i < num; i++) {
 			ACard card = null;
 			list.add(card);
 		}
@@ -47,5 +47,10 @@ public class LogicTestCases {
 	@Test
 	public void testForOne(){
 		Assert.assertEquals(generateAvailableCards(1),generateTestPlayer().getAvailableCards(generateTestCards(1)));
+	}
+	
+	@Test
+	public void testForTen(){
+		Assert.assertEquals(generateAvailableCards(10),generateTestPlayer().getAvailableCards(generateTestCards(10)));
 	}
 }
