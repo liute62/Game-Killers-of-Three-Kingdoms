592b3f42610c3bc896aff8f795a01cc3f8f86250
Max_Liu <liute62@sina.cn>
2015-04-03 15:53:44 -0400

    Change the method and successfully satisfy this circumstance that if a player in a castcard state, he can not use the dodge card generally.

--- Killers of Three Kindoms/src/com/logic/player/APlayer.java
+++ Killers of Three Kindoms/src/com/logic/player/APlayer.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import com.card.interfaces.ACard;
 import com.logic.interfaces.IPlayer;
+import com.system.constants.CardConst;
 import com.system.enums.GameState;
 
 public abstract class APlayer implements IPlayer{
@@ -23,6 +24,14 @@ public abstract class APlayer implements IPlayer{
 	 public List<ACard> getAvailableCards(List<ACard> cards) {
 		 if(gameState == GameState.begin || gameState == gameState.check || gameState == GameState.drawCard){
 			 return new ArrayList<ACard>();
+		 }if(gameState == GameState.castCard){
+			 List<ACard> listType = new ArrayList<ACard>();
+			 for (ACard c : cards) {
+					if (c != null && c.getType() == CardConst.CardType_Dodge) {
+						listType.add(c);
+					}
+				}
+			 cards.removeAll(listType);
 		 }
 		return cards;
 	}
