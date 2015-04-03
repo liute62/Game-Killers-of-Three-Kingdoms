1f896eacbdc3b075638a3249afa374626d60aa60
XTLi <xtli1689@163.com>
2015-04-03 14:15:42 -0400

    add testLose2HP

--- Killers of Three Kindoms/src/com/logic/test/LogicMainTestCase.java
+++ Killers of Three Kindoms/src/com/logic/test/LogicMainTestCase.java
@@ -10,10 +10,19 @@ public class LogicMainTestCase {
 	Player player = new Player();
 	
 	
-	@Test public void testLose1HP()
+	@Test 
+	public void testLose1HP()
 	{
 		player.setCurrentHP(4);
 		player.loseHP(1);
 		assertEquals(3, player.getCurrentHP());
+	}
+	
+	@Test 
+	public void testLose2HP()
+	{
+		player.setCurrentHP(4);
+		player.loseHP(2);
+		assertEquals(2, player.getCurrentHP());
 	}
 }
