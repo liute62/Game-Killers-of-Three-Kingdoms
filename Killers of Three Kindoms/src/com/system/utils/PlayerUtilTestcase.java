6cca0fae45c41b05e360fe9220209583803b9f0d
zxqdx <acfun.zxqdx@gmail.com>
2015-04-17 01:26:34 -0400

    Added a failing test case testGetDistanceWorksForDistanceOne()

--- Killers of Three Kindoms/src/com/system/utils/PlayerUtilTestcase.java
+++ Killers of Three Kindoms/src/com/system/utils/PlayerUtilTestcase.java
@@ -1,13 +1,26 @@
 package com.system.utils;
 
+import com.logic.player.Player;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class PlayerUtilTestcase {
 
+    private PlayerUtil playerUtil;
+    
+    @Before
+    public void setUp() {
+        playerUtil = new PlayerUtil();
+    }
+    
     @Test
-    public void testShuffleCard() {
-        
+    public void testGetDistanceWorksForDistanceOne() {
+        Player player1 = new Player();
+        Player player2 = new Player();
+        player1.setPosition(1);
+        player2.setPosition(2);
+        assertEquals(playerUtil.getDistance(player1, player2), 1);
     }
 }
