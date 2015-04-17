964753638458a9dc6cf67aa6ca482aac60966163
zxqdx <acfun.zxqdx@gmail.com>
2015-04-17 02:39:39 -0400

    Added test that happens to pass: testStrikeOnePlayerInRange()

--- Killers of Three Kindoms/src/com/card/test/CardMainTestCase.java
+++ Killers of Three Kindoms/src/com/card/test/CardMainTestCase.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import com.card.base.StrikeCard;
 import org.junit.Test;
 
 import com.card.base.PeachCard;
@@ -14,57 +15,75 @@ import com.logic.player.APlayer;
 import com.logic.player.Player;
 
 public class CardMainTestCase {
-	
-	private List<Integer> list(Integer...integers) {
-		return Arrays.asList(integers);
-	}
 
-	@Test
-	public void testForEightDiagramFormationCard() {
-		EightDiagramFormationCard card = new EightDiagramFormationCard();
-		assertEquals(true,card.check("Heart"));
-		assertEquals(true,card.check("Diamond"));
-		assertEquals(false,card.check("Club"));
-		assertEquals(false,card.check("Spade"));
-	}
-	
-	@Test
-	public void testUsingPeachCardForOnePlayer() {
-		PeachCard peach = new PeachCard();
-		APlayer p1 = new Player();
-		APlayer p2 = new Player();
-		p2.setCurrentHP(1);
-		List<APlayer> targetPlayers = new ArrayList<APlayer>();
-		targetPlayers.add(p2);
-		
-		List<Integer> healthList = new ArrayList<Integer>();
-		peach.use(p1, targetPlayers);
-		for(APlayer targetPlayer: targetPlayers)
-		{
-			healthList.add(targetPlayer.getCurrentHP());
-		}
-		assertEquals(list(2), healthList);
-	}
+    private List<Integer> list(Integer...integers) {
+        return Arrays.asList(integers);
+    }
 
-	@Test
-	public void testUsingPeachCardForTwoPlayers() {
-		PeachCard peach = new PeachCard();
-		APlayer p1 = new Player();
-		APlayer p2 = new Player();
-		p1.setCurrentHP(2);
-		p2.setCurrentHP(1);
-		List<APlayer> targetPlayers = new ArrayList<APlayer>();
-		
-		targetPlayers.add(p1);
-		targetPlayers.add(p2);
-		
-		List<Integer> healthList = new ArrayList<Integer>();
-		peach.use(p1, targetPlayers);
-		for(APlayer targetPlayer: targetPlayers)
-		{
-			healthList.add(targetPlayer.getCurrentHP());
-		}
-		assertEquals(list(3,2), healthList);
-	}
+    @Test
+    public void testForEightDiagramFormationCard() {
+        EightDiagramFormationCard card = new EightDiagramFormationCard();
+        assertEquals(true,card.check("Heart"));
+        assertEquals(true,card.check("Diamond"));
+        assertEquals(false,card.check("Club"));
+        assertEquals(false,card.check("Spade"));
+    }
+
+    @Test
+    public void testUsingPeachCardForOnePlayer() {
+        PeachCard peach = new PeachCard();
+        APlayer p1 = new Player();
+        APlayer p2 = new Player();
+        p2.setCurrentHP(1);
+        List<APlayer> targetPlayers = new ArrayList<APlayer>();
+        targetPlayers.add(p2);
+
+        List<Integer> healthList = new ArrayList<Integer>();
+        peach.use(p1, targetPlayers);
+        for(APlayer targetPlayer: targetPlayers)
+        {
+            healthList.add(targetPlayer.getCurrentHP());
+        }
+        assertEquals(list(2), healthList);
+    }
+
+    @Test
+    public void testUsingPeachCardForTwoPlayers() {
+        PeachCard peach = new PeachCard();
+        APlayer p1 = new Player();
+        APlayer p2 = new Player();
+        p1.setCurrentHP(2);
+        p2.setCurrentHP(1);
+        List<APlayer> targetPlayers = new ArrayList<APlayer>();
+
+        targetPlayers.add(p1);
+        targetPlayers.add(p2);
+
+        List<Integer> healthList = new ArrayList<Integer>();
+        peach.use(p1, targetPlayers);
+        for(APlayer targetPlayer: targetPlayers)
+        {
+            healthList.add(targetPlayer.getCurrentHP());
+        }
+        assertEquals(list(3,2), healthList);
+    }
+
+    @Test
+    public void testStrikeOnePlayerInRange() {
+        StrikeCard strike = new StrikeCard();
+        APlayer player = new Player();
+        APlayer player1 = new Player();
+        APlayer player2 = new Player();
+        player.setCurrentHP(5);
+        player.setAttackRange(2);
+        player.setPosition(1);
+        player1.setCurrentHP(5);
+        player1.setPosition(2);
+        List<APlayer> playerList = new ArrayList<>();
+        playerList.add(player1);
+        assertEquals(strike.checkTarget(playerList), true);
+    }
+
+    
 
 }
