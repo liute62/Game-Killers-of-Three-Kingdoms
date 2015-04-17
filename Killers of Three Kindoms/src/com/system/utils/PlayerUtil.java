bec15e430ad523f389ed8673c9fce376d5f4b666
zxqdx <acfun.zxqdx@gmail.com>
2015-04-17 01:36:48 -0400

    Fixed failing test: testGetDistanceWorksForAnotherDistanceOne()

--- Killers of Three Kindoms/src/com/system/utils/PlayerUtil.java
+++ Killers of Three Kindoms/src/com/system/utils/PlayerUtil.java
@@ -16,6 +16,6 @@ public class PlayerUtil {
      * @throws java.lang.RuntimeException if distance is not valid.
      */
     public int getDistance(APlayer player1, APlayer player2) {
-        return (player2.getPosition() - 1);
+        return (player2.getPosition() - player1.getPosition());
     }
 }
