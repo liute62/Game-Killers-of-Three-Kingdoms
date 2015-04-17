c6c771c763c72a29c0c869bf78055c5fa1cb846e
XTLi <xtli1689@163.com>
2015-04-17 15:00:59 -0400

    Modify to satisfy testAmorCardCanCorrectlyUse

--- Killers of Three Kindoms/src/com/card/interfaces/AmorCard.java
+++ Killers of Three Kindoms/src/com/card/interfaces/AmorCard.java
@@ -6,7 +6,10 @@ import com.logic.player.APlayer;
 
 public abstract class AmorCard extends AEquipmentCard{
 	public void use(APlayer player,List<APlayer> target) {
-		
+		for(APlayer targetPlayer: target)
+		{
+			targetPlayer.setAmor(this);
+		}
 	}
 	
 	
