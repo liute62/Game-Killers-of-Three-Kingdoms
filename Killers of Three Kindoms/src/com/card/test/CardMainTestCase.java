0bbbf683248522f0b2b446f4ab929cfa448ad729
zxqdx <acfun.zxqdx@gmail.com>
2015-04-23 21:49:09 -0400

    Added failing test TestPlayerWithoutQilinAttackTargetWithMinusMount

--- Killers of Three Kindoms/src/com/card/test/CardMainTestCase.java
+++ Killers of Three Kindoms/src/com/card/test/CardMainTestCase.java
@@ -7,16 +7,13 @@ import java.util.Arrays;
 import java.util.List;
 
 import com.card.base.StrikeCard;
-import com.card.equipment.QilinBowCard;
-import com.card.equipment.RedHareCard;
-import com.card.equipment.ShadowRunnerCard;
+import com.card.equipment.*;
 import com.card.interfaces.MinusMountCard;
 import com.card.interfaces.PlusMountCard;
 import com.card.interfaces.WeaponCard;
 import org.junit.Test;
 
 import com.card.base.PeachCard;
-import com.card.equipment.EightDiagramFormationCard;
 import com.logic.player.APlayer;
 import com.logic.player.Player;
 
@@ -276,5 +273,27 @@ public class CardMainTestCase {
 
         strike.use(player, playerList);
         assertNull(player1.getMinusMount());
+    }
+
+    @Test
+    public void TestPlayerWithoutQilinAttackTargetWithMinusMount() {
+        WeaponCard blackPommel = new BlackPommelCard();
+        StrikeCard strike = new StrikeCard();
+        MinusMountCard redHare = new RedHareCard();
+
+        APlayer player = new Player();
+        APlayer player1 = new Player();
+        player.setCurrentHP(3);
+        player.setAttackRange(2);
+        player.setPosition(1);
+        player.setWeapon(blackPommel);
+        player1.setCurrentHP(2);
+        player1.setPosition(2);
+        player1.setMinusMount(redHare);
+        List<APlayer> playerList = new ArrayList<>();
+        playerList.add(player1);
+
+        strike.use(player, playerList);
+        assertSame(player1.getMinusMount(), redHare);
     }
 }
