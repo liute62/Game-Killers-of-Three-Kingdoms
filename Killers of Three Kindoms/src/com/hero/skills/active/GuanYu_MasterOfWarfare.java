576d168af304e7375de1767cb7562281d207f30b
zxqdx <acfun.zxqdx@gmail.com>
2015-04-30 23:01:45 -0400

    Fixed failing test testGuanYuCanNotUseBlackCardToActivateSkill

--- Killers of Three Kindoms/src/com/hero/skills/active/GuanYu_MasterOfWarfare.java
+++ Killers of Three Kindoms/src/com/hero/skills/active/GuanYu_MasterOfWarfare.java
@@ -3,6 +3,7 @@ package com.hero.skills.active;
 import com.card.interfaces.ACard;
 import com.hero.skills.interfaces.ISkill;
 import com.logic.player.APlayer;
+import com.system.constants.SuitConst;
 import com.system.enums.HeroName;
 
 import java.util.List;
@@ -16,6 +17,6 @@ public class GuanYu_MasterOfWarfare implements ISkill {
 
     @Override
     public boolean check(APlayer operator, List<ACard> cards, List<APlayer> players) {
-        return operator.getName().equals(HeroName.GuanYu);
+        return operator.getName().equals(HeroName.GuanYu) && cards.get(0).getSuit() != SuitConst.SuitType_Spades;
     }
 }
