c005be9f68247d1de23623012dc668a3b53987b1
zxqdx <acfun.zxqdx@gmail.com>
2015-05-13 14:31:59 -0400

    Added test case for testAllCardsGenerated

--- Killers of Three Kindoms/src/com/system/test/CardUtilOtherTestcase.java
+++ Killers of Three Kindoms/src/com/system/test/CardUtilOtherTestcase.java
@@ -1,5 +1,30 @@
 package com.system.test;
 
+import com.card.interfaces.ACard;
+import com.system.utils.CardUtil;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 public class CardUtilOtherTestcase {
-    
+    private void printCards(List<ACard> cards) {
+        for (int i = 0, len = cards.size(); i < len; i++) {
+            System.out.print(cards.get(i).getName() + ", ");
+            if ((i + 1) % 10 == 0) {
+                System.out.println();
+            }
+        }
+        System.out.println();
+    }
+
+    @Test
+    public void testAllCardsGenerated() {
+        CardUtil cardUtil = CardUtil.getInstance();
+        List<ACard> cards = cardUtil.generateInitialCardDeck();
+        assertEquals(53, cards.size());
+        printCards(cards);
+    }
 }
