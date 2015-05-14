ca9b6738349c19b1c57eb968f5285c377dd1424f
XTLi <xtli1689@163.com>
2015-05-14 16:34:12 -0400

    TDD: Add testOtherPlayerCannotUseAssult, but fail

--- Killers of Three Kindoms/src/com/hero/test/HeroSkillTest.java
+++ Killers of Three Kindoms/src/com/hero/test/HeroSkillTest.java
@@ -112,5 +112,19 @@ public class HeroSkillTest {
 		Assert.assertEquals(true, p1.checkSkill(Arrays.asList(card), Arrays.asList(p2,p3)));
 	}
 	
+	@Test
+	public void testOtherPlayerCannotUseAssult()
+	{
+		APlayer p1 = new Player();
+		APlayer p2 = new Player();
+		APlayer p3 = new Player();
+		ACard card = null;
+		p1.gameStage = GameStage.discard;
+		p1.setName(HeroName.MaChao);
+		ISkill t = new ZhangLiao_Assault();
+		p1.setSkill(t);
+		Assert.assertEquals(false, p1.checkSkill(Arrays.asList(card), Arrays.asList(p2,p3)));
+	}
+	
 	
 }
