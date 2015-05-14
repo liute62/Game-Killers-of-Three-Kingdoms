4d41a91f0ad5e8a9e6e618cc2ebe1d5be2e95815
XTLi <xtli1689@163.com>
2015-05-14 19:14:21 -0400

    TDD: Modify to satisfy

--- Killers of Three Kindoms/src/com/hero/skills/active/ZhangLiao_Assault.java
+++ Killers of Three Kindoms/src/com/hero/skills/active/ZhangLiao_Assault.java
@@ -14,6 +14,22 @@ public class ZhangLiao_Assault implements ISkill{
 	@Override
 	public void use(APlayer operator, List<ACard> cards, List<APlayer> players) {
 		// TODO Auto-generated method stub
+		Random random = new Random();
+		for(APlayer player: players)
+		{
+			if(player.getHands().size() == 1)
+			{
+				operator.getHands().add(player.getHands().get(0));
+			}
+			else 
+			{
+				int min = 0;
+				int max = player.getHands().size()-1;
+				int randomIndex = random.nextInt(max)%(max-min+1) + min;
+				operator.getHands().add(player.getHands().get(randomIndex));
+			}
+			
+		}
 	}
 
 	@Override
