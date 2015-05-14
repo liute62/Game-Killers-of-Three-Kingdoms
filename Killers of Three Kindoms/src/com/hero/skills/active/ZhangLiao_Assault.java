28eb0667918eafd44734df8442d088f04ffd8690
XTLi <xtli1689@163.com>
2015-05-14 16:30:48 -0400

    Modify to satisfy

---
+++ Killers of Three Kindoms/src/com/hero/skills/active/ZhangLiao_Assault.java
@@ -0,0 +1,25 @@
+package com.hero.skills.active;
+
+import java.util.List;
+import java.util.Random;
+
+import com.card.interfaces.ACard;
+import com.hero.skills.interfaces.ISkill;
+import com.logic.player.APlayer;
+
+public class ZhangLiao_Assault implements ISkill{
+
+	@Override
+	public void use(APlayer operator, List<ACard> cards, List<APlayer> players) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public boolean check(APlayer operator, List<ACard> cards,
+			List<APlayer> players) {
+		// TODO Auto-generated method stub
+	}
+	
+
+}
+
